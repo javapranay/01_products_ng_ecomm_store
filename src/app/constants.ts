017caaa1034c7eda7977b88a05a2ee56fd086dea
javapranay <62851245+javapranay@users.noreply.github.com>
2024-12-16 16:43:45 +0530

    Update constants.ts

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,3 @@
 export const Constants = {
-    BACKEND_API_URL: 'http://a5a0029d2571e4f3495ab51500f19e55-1888408116.ap-south-1.elb.amazonaws.com',
+    BACKEND_API_URL: 'http://ab5b802eb7569484d94a43a0b0c3d70e-163394980.ap-south-1.elb.amazonaws.com',
 };
